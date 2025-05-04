41a82752a88473697c2e3d07c6e55d2ddc56a93c
naoko-asano <asano@wyrd.co.jp>
2025-05-04 11:35:50 +0900

    fix: CORSエラーの解消

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, jsonify
+import os
 import board
 import busio
 import adafruit_sht31d
+from flask import Flask, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app, origins=[os.getenv("BABY_MONITOR_URL")])
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_sht31d.SHT31D(i2c)
